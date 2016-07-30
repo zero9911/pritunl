2645dca71a089176d76549e58c5e39dbe23c4837
zero9911 <zerolast9911@gmail.com>
2016-07-30 20:24:49 +0800

    Update debian8.sh

--- debian8.sh
+++ debian8.sh
@@ -8,9 +8,9 @@ apt-get -y install ufw
 apt-get -y install sudo
 
 # Install Pritunl
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" > /etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 echo "deb http://repo.pritunl.com/stable/apt jessie main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv EA312927
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
 apt-get -y update
 apt-get -y upgrade
