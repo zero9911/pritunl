9fe4f776844eac99fe5df81b6b6986da3bd0a95d
zero9911 <zerolast9911@gmail.com>
2016-07-30 20:25:18 +0800

    Update ubuntu14.sh

--- ubuntu14.sh
+++ ubuntu14.sh
@@ -5,9 +5,9 @@ cd
 
 # Install Pritunl
 #!/bin/bash
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv EA312927
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
 apt-get --assume-yes update
 apt-get --assume-yes install pritunl mongodb-org
